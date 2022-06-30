511e4019069139a8c9a396e069e9912d51eac6a2
Georgiy Goloborodov <barstyle@mail.ru>
2022-06-30 13:49:03 +0300

    Initial commit

---
+++ src/main/java/config/JavaConfig.java
@@ -0,0 +1,62 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement // если все пошло не по плану делает ролбэк
+@ComponentScan(value = "java")
+
+// 4. Конфигурация Spring через JavaConfig и аннотации, по аналогии с предыдущими проектами.
+// Без использования xml. Без Spring Boot.
+public class JavaConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(environment.getProperty("db.url"));
+        driverManagerDataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+        driverManagerDataSource.setUsername(environment.getProperty("db.username"));
+        driverManagerDataSource.setPassword(environment.getProperty("db.password"));
+        return driverManagerDataSource;
+    }
+
+    // 5. Внесите изменения в конфигурацию для работы с базой данных.
+    // Вместо SessionFactory должен использоваться EntityManager.
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(getDataSource());
+
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+
+        localSessionFactoryBean.setHibernateProperties(properties);
+        // localSessionFactoryBean.setAnnotatedClasses(User.class);
+        return localSessionFactoryBean;
+    }
+
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
+    }
+
+
+}
